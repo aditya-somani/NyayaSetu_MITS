17cde87e933f1d0213619cbb167e52b73bf93173
Atul <atulkhiyani09@gmail.com>
2025-07-10 02:25:34 +0530

    changes fixed

--- NyayaSetu_MITS/backend/src/app.js
+++ NyayaSetu_MITS/backend/src/app.js
@@ -7,12 +7,17 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
-  origin: "*", // ✅ your frontend URL
-  credentials: true,               // ✅ important!
+  origin: 'https://nyayasetu-mits-1.onrender.com',  // ✅ Replace with your frontend domain
+  credentials: true,                                // ✅ Allow cookies
 }));
+
 app.use(express.static('public'))
 app.use(cookieParser())
 
+app.use((req, res, next) => {
+  console.log(`[${req.method}] ${req.originalUrl}`);
+  next();
+});
 
 
 import ticketRoutes from './routers/ticket.routes.js'
