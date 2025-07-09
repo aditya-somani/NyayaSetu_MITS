129ad6b7ec9aeff11cde034677c8a81d4d133294
Atul <atulkhiyani09@gmail.com>
2025-07-09 23:21:25 +0530

    Fixed issue

--- NyayaSetu_MITS/backend/src/app.js
+++ NyayaSetu_MITS/backend/src/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
-  origin: "http://localhost:8080", // ✅ your frontend URL
+  origin: "https://nyayasetu-mits-1.onrender.com/", // ✅ your frontend URL
   credentials: true,               // ✅ important!
 }));
 app.use(express.static('public'))
